5125d270cea52d33ce565eaf2ff8675c8750fe0a
pianndi <demakalviandi@gmail.com>
2024-03-05 09:30:14 +0700

    first

---
+++ database/migrations/2024_03_05_021224_create_quizzes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('category_id');
+            $table->timestamp('schedule')->nullable();
+            $table->boolean('random');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
---
+++ database/migrations/2024_03_05_021444_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('quiz_id');
+            $table->string('choice_1');
+            $table->string('choice_2');
+            $table->string('choice_3');
+            $table->string('choice_4');
+            $table->string('choice_5');
+            $table->integer('answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
